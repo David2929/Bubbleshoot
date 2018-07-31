7e47dfdbb6029edd3b9ee8a260afd1aee86c3915
David Quinn <dquinn29@me.com>
2018-07-31 13:11:15 -0400

    file updates

---
+++ _js/ui.js
@@ -0,0 +1,11 @@
+var BubbleShoot = window.Bubbleshoot || {};
+BubbleShoot.ui = (function($){
+	var ui = {
+		init : function(){
+		},
+	hideDialog : function(){
+		$(".dialog").fadeOut(300);
+	}
+};
+return ui;
+})(jQuery);
