9cc76f30b3ceafaba451370a2a4dfa2fcf724274
David Quinn <dquinn29@me.com>
2018-08-03 12:00:22 -0400

    chapter 2 changes

--- _js/ui.js
+++ _js/ui.js
@@ -1,10 +1,32 @@
 var BubbleShoot = window.Bubbleshoot || {};
 BubbleShoot.ui = (function($){
 	var ui = {
+		BUBBLE DIMS : 44
 		init : function(){
 		},
 	hideDialog : function(){
 		$(".dialog").fadeOut(300);
+	},
+	getMouseCoords : function(e){
+		var coords = {x : e.pageX, y : e.pageY};
+		return coords;
+	},
+	getBubbleCoords : function(bubble){
+		var bubbleCoords = bubble.position();
+		bubbleCoords.left += ui.BUBBLE_DIMS/2;
+		bubbleCoords.top += ui.BUBBLE_DIMS/2;
+		return bubbleCoords;
+	},
+	getBubbleAngle : function(bubble,e){
+		var mouseCoords = ui.getMouseCoords(e);
+		var bubbleCoords = ui.getBubbleCoords(bubble);
+		var gameCoords = $("game").position();
+		var boardLeft = 120;
+		var angle = Math.atan((mouseCoords.x - bubbleCoords.left - boardLeft) / (bubbleCoords.top + gameCoords.top - mouseCoords.y));
+		if(mouseCoords.y > bubbleCoords.top + gameCoords.top){
+			angle += Math.PI;
+		}
+		return angle;
 	}
 };
 return ui;
