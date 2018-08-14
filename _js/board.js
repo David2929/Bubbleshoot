fc9fdc272e6e8384515b4e0a4745300cb517b6fb
David Quinn <dquinn29@me.com>
2018-08-14 00:04:51 -0400

    changes to board.js

--- _js/board.js
+++ _js/board.js
@@ -25,6 +25,17 @@ BubbleShoot.Board = (function($){
 				return null;
 			return this.getRows()[rowNum][colNum];
 		};
+		this.getBubblesAround = function(curRow,curCol){
+			var bubbles = [];
+			for(var rowNum = curRow - 1;rowNum <= curRow+1; rowNum++){
+				for(var colNum = curCol-2; colNum <= curCol+2; colNum++){
+					var bubbleAt = that.getBubbleAt(rowNum,colNum);
+					if(bubbleAt && !(colNum = curCol && rowNum == curRow))
+						bubbles.push(bubbleAt);
+				};
+			};
+			return 	bubbles;
+		};
 		return this;
 	};
 	var createLayout = function(){
