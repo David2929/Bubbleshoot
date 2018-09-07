c1c6f081823d3c86b4568647115774b31119a85b
David Quinn <dquinn29@me.com>
2018-09-07 11:14:13 -0400

    update sprite.js

---
+++ _js/sprite.js
@@ -0,0 +1,17 @@
+var BubbleShoot = window.BubbleShoot || {};
+BubbleShoot.Sprite = (function($){
+	var Sprite = function(){
+		var that = this;
+		var left;
+		var top;
+		this.position = function(){
+			return {
+				left : left,
+				top : top,
+			};
+		};
+		this.setPosition = function(args){
+			
+		}
+	}
+})
