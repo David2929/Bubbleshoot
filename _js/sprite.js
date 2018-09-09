29f69d1f04cd424fe432a6ad70ef28710d07781b
David Quinn <dquinn29@me.com>
2018-09-08 21:52:52 -0400

    update sprite.js

--- _js/sprite.js
+++ _js/sprite.js
@@ -11,7 +11,28 @@ BubbleShoot.Sprite = (function($){
 			};
 		};
 		this.setPosition = function(args){
-			
-		}
-	}
-})
+			if(arguments.length > 1){
+				return;
+			};
+			if(args.left !== null)
+				left = args.left;
+			if(args.top !== null)
+				top = args.top;
+		};
+		this.css = this.setPosition;
+		return this;
+	};
+	Sprite.prototype.width = function(){
+		return BubbleShoot.ui.BUBBLE_DIMS;
+	};
+	Sprite.prototype.height = function(){
+		return BubbleShoot.ui.BUBBLE_DIMS;
+	};
+	Sprite.prototype.removeClass = function(){};
+	Sprite.prototype.addClass = function(){};
+	Sprite.prototype.remove = function(){};
+	Sprite.prototype.kaboom = function(){
+		jQuery.fn.kaboom.apply(this);
+	};
+	return Sprite;
+})(jQuery);
