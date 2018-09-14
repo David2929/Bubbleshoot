dd1308bb78f3ff9d8a00ab4ac2d78bb3113e4cb3
David Quinn <dquinn29@me.com>
2018-09-14 01:21:06 -0400

    update renderer.js

--- _js/renderer.js
+++ _js/renderer.js
@@ -2,6 +2,8 @@ var BubbleShoot = window.BubbleShoot || {};
 BubbleShoot.renderer = (function($){
 	var canvas;
 	var context;
+	var spriteSheet;
+	var BUBBLE_IMAGE_DIM = 50;
 	var Rendered = {
 		init : function(callback){
 			canvas = document.createElement("canvas");
@@ -10,7 +12,11 @@ BubbleShoot.renderer = (function($){
 			$(canvas).attr("width",$(canvas).width());
 			$(canvas).attr("height",$(canvas).height());
 			context = canvas.getContext("2d");
+			spriteSheet = new Image();
+			spriteSheet.src = "_img/bubble_sprite_sheet.png";
+			spriteSheet.onload = function(){
 			callback();
+			};
 		}
 	};
 	return Renderer;
