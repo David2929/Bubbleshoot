862aa1595b991ca4d59550b71a65d07d63bad392
David Quinn <dquinn29@me.com>
2018-10-26 20:12:42 -0400

    correct renderer.js

--- _js/renderer.js
+++ _js/renderer.js
@@ -1,10 +1,10 @@
 var BubbleShoot = window.BubbleShoot || {};
-BubbleShoot.renderer = (function($){
+BubbleShoot.Renderer = (function($){
 	var canvas;
 	var context;
 	var spriteSheet;
 	var BUBBLE_IMAGE_DIM = 50;
-	var Rendered = {
+	var Renderer = {
 		init : function(callback){
 			canvas = document.createElement("canvas");
 			$(canvas).addClass("game_canvas");
@@ -14,50 +14,49 @@ BubbleShoot.renderer = (function($){
 			context = canvas.getContext("2d");
 			spriteSheet = new Image();
 			spriteSheet.src = "_img/bubble_sprite_sheet.png";
-			spriteSheet.onload = function(){
-			callback();
+			spriteSheet.onload = function() {
+				callback();
 			};
-			render : function(bubbles){
-				context.clearRect(0,0,canvas.width,canvas.height);
-				context.translate(120,0);
-				$.each(bubbles,function(){
-					var bubble = this;
-					var clip = {
-						top : bubble.getType() * BUBBLE_IMAGE_DIM,
-						left : 0
-					};
+		},
+		render : function(bubbles){
+			context.clearRect(0,0,canvas.width,canvas.height);
+			context.translate(120,0);
+			$.each(bubbles,function(){
+				var bubble = this;
+				var clip = {
+					top : bubble.getType() * BUBBLE_IMAGE_DIM,
+					left : 0
+				};
 					switch(bubble.getState()){
 						case BubbleShoot.BubbleState.POPPING:
 							var timeInState = bubble.getTimeInState();
 							if(timeInState < 80){
 								clip.left = BUBBLE_IMAGE_DIM;
-							}else if(timeInState <1 40){
+								}else if(timeInState < 140){
 								clip.left = BUBBLE_IMAGE_DIM*2;
-							}else{
+								}else{
 								clip.left = BUBBLE_IMAGE_DIM*3;
 							};
 							break;
-							case BubbleShoot.BubbleState.POPPED:
-								return;
-							case BubbleShoot.BubbleState.FIRED:
-								return;
-							case BubbleShoot.BubbleState.FALLEN:
-								return;
-							}
-
-					Renderer.drawSprite(bubble.getSprite(),clip);
-				});
-				context.translate(-120,0);
-			},
-			drawSprite : function(sprite,clip){
-				context.translate(sprite.position().left + sprite.width()/2,sprite.
-					position().top + sprite.height()/2);
-				contect.drawImage(spriteSheet,clip.left,clip.top,BUBBLE_IMAGE_DIM,
-					BUBBLE_IMAGE_DIM,-sprite.width()/2,-sprite.height()/2,BUBBLE_IMAGE_
-					DIM,BUBBLE_IMAGE_DIM);
-				context.translate(-sprite.position().left - sprite.width()/2,
-					-sprite.position().top - sprite.height()/2); 
-			}
+						case BubbleShoot.BubbleState.POPPED:
+							return;
+						case BubbleShoot.BubbleState.FIRED:
+							return;
+						case BubbleShoot.BubbleState.FALLEN:
+							return;
+					}
+				Renderer.drawSprite(bubble.getSprite(),clip);
+			});
+			context.translate(-120,0);
+		},
+		drawSprite : function(sprite,clip){
+			context.translate(sprite.position().left + sprite.width()/2,sprite.
+				position().top + sprite.height()/2);
+			context.drawImage(spriteSheet,clip.left,clip.top,BUBBLE_IMAGE_DIM,
+				BUBBLE_IMAGE_DIM,-sprite.width()/2,-sprite.height()/2,BUBBLE_IMAGE_DIM,
+				BUBBLE_IMAGE_DIM);
+			context.translate(-sprite.position().left - sprite.width()/2,
+				-sprite.position().top - sprite.height()/2);
 		}
 	};
 	return Renderer;
